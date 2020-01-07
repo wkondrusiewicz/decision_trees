486d8a78aad0f83a7b5e0ae08740dfca23814782
Wiktor <vikkon00@gmail.com>
2020-01-07 16:20:22 +0100

    add scoring func for regression

--- decision_trees/metrics.py
+++ decision_trees/metrics.py
@@ -22,7 +22,7 @@ class BaseScorer:
 
 class GiniImportance(BaseScorer):
     def __init__(self):
-        pass
+        super().__init__()
 
     @staticmethod
     def scoring_condition(previous_value, new_value):
@@ -42,3 +42,26 @@ class GiniImportance(BaseScorer):
         left_gini = self.get_score(y_left)
         right_gini = self.get_score(y_right)
         return (len(y_left) * left_gini + len(y_right) * right_gini) / len(y)
+
+
+class MeanSquaredError(BaseScorer):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def scoring_condition(previous_value, new_value):
+        return previous_value < new_value
+
+    @staticmethod
+    def get_score(y):
+        return np.sum(np.array(y)**2)
+        return gini
+
+    def get_score_after_split(self, X, y, threshold):
+        left_mask = X < threshold
+        right_mask = X >= threshold
+        y_left = y[left_mask]
+        y_right = y[right_mask]
+        left_mse = self.get_score(y_left)
+        right_mse = self.get_score(y_right)
+        return (len(y_left) * left_mse + len(y_right) * right_mse) / len(y)
