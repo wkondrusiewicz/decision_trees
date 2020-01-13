6169cc47accc711ea244fbda384e23a03bbe2a73
Wiktor <vikkon00@gmail.com>
2020-01-13 16:30:58 +0100

    cleanup

--- decision_trees/tree.py
+++ decision_trees/tree.py
@@ -7,11 +7,15 @@ from decision_trees.node import TreeNode
 from decision_trees.metrics import GiniImportance, StandardDeviationReduction
 
 
-class BasicTree:
-    def __init__(self, max_depth, scorer=GiniImportance):
+class BasicTreePredictor:
+    def __init__(self, max_depth, scorer, mode):
+        assert mode in ['classification',
+                        'regression'], 'Invalid mode type'
         self.__max_depth = max_depth
         self.scorer = scorer()  # create an instance of scorer class
+        self.mode = mode
         self.tree = None
+        self.number_of_classes = None
 
     @property
     def max_depth(self):
@@ -21,248 +25,146 @@ class BasicTree:
     def max_depth(self, max_depth):
         self.__max_depth = max_depth
 
-    def fit(self, X, y):
-        pass
-
-    def predict(self, X):
-        pass
-
     def _get_best_split_for_feature(self, X_slice, y_slice):
-        pass
+        X_slice_sorted = np.sort(X_slice)
+        thresholds = np.unique(
+            (X_slice_sorted[1:] + X_slice_sorted[:-1]) / 2)
+        current_fueature_score = self.scorer.get_score(y_slice)
+        best_score = current_fueature_score
+        best_threshold = None
+        if len(y_slice) <= 1:  # >= 2 elements are needed to split a Node
+            return None, None
+        for thresh in thresholds:
+            score = self.scorer.get_score_after_split(
+                X_slice, y_slice, thresh)
+            if score < best_score:
+                best_score = score
+                best_threshold = thresh
+        return best_score, best_threshold
 
     def _get_best_split_for_dataset(self, X, y):
-        pass
+        current_fueature_score = self.scorer.get_score(y)
+        best_score = current_fueature_score
+        for i in range(X.shape[1]):
+            X_slice = X[:, i]
+            best_score_for_feature, best_thresh_for_feature = self._get_best_split_for_feature(
+                X_slice, y)
+            if best_thresh_for_feature is not None and self.scorer.scoring_condition(best_score_for_feature, best_score):
+                best_score = best_score_for_feature
+                best_index = i
+                best_thresh = best_thresh_for_feature
+            elif best_thresh_for_feature is None:
+                best_index = None
+                best_thresh = None
+        return best_score, best_index, best_thresh
 
-    def _grow_tree(self, X, y, depth=0):
-        pass
+    def _grow_tree(self, X, y, depth=0, tree_structure={}):
+        if depth == 0 and len(tree_structure) == 0:
+            tree_structure = {}
+
+        current_score = self.scorer.get_score(y)
+        if self.mode == 'classification':
+            samples_per_class = [np.sum(y == i)
+                                 for i in range(1, self.number_of_classes + 1)]
+            prediction = np.argmax(samples_per_class) + 1
+        elif self.mode == 'regression':
+            samples_per_class = None
+            prediction = np.mean(y)
+
+        tree_node = TreeNode(
+            score=current_score, samples_per_class=samples_per_class, prediction=prediction)
+        if depth < self.max_depth:
+            best_score, best_index, best_thresh = self._get_best_split_for_dataset(
+                X, y)
+            if best_index is not None and best_thresh is not None:
+                left_mask = X[:, best_index] < best_thresh
+                right_mask = X[:, best_index] >= best_thresh
+                X_left = X[left_mask]
+                X_right = X[right_mask]
+                y_left = y[left_mask]
+                y_right = y[right_mask]
+                tree_node.feature_index = best_index
+                tree_node.threshold = best_thresh
+                root_key = self.get_output_format(
+                    best_score, samples_per_class, prediction, best_index, best_thresh)
+                tree_structure[root_key] = {}
+                tree_node.tree_structure = tree_structure
+                tree_node.left_node = self._grow_tree(
+                    X_left, y_left, depth + 1, tree_structure[root_key])
+                tree_node.right_node = self._grow_tree(
+                    X_right, y_right, depth + 1, tree_structure[root_key])
+        else:
+            root_key = self.get_output_format(
+                current_score, samples_per_class, prediction)
+            tree_structure[root_key] = {}
+            tree_node.tree_structure = tree_structure
+        return tree_node
+
+    def get_output_format(self, score, samples_per_class, prediction, index=None, threshold=None):
+        root_key = f'{type(self.scorer).__name__} = {score:.2f}\n'
+        if self.mode == 'classification':
+            root_key += f'samples = {np.sum(samples_per_class)}\nclass division = {samples_per_class}\n'
+        root_key += f'prediction = {prediction}'
+        if threshold is not None and index is not None:
+            root_key = f'feature_{index} < {threshold:.2f}\n' + root_key
+        return root_key
+
+    def fit(self, X, y):
+        self.tree = self._grow_tree(X, y)
+
+    def _get_single_prediction(self, X_single):
+        assert self.tree is not None, 'Please first fit a tree to the data'
+
+        node = self.tree
+        while node.left_node:
+            if X_single[node.feature_index] < node.threshold:
+                node = node.left_node
+            else:
+                node = node.right_node
+        return node.prediction
+
+    def predict(self, X):
+        return [self._get_single_prediction(x) for x in X]
+
+    def draw_tree(self, out_dir):
+        assert self.tree is not None, 'Please first fit a tree to the data'
+
+        graph = pydot.Dot(graph_type='graph')
+
+        def draw(parent_name, child_name):
+            edge = pydot.Edge(parent_name, child_name)
+            graph.add_edge(edge)
+
+        def visit(node, parent=None):
+            for k, v in node.items():
+                if isinstance(v, dict):
+                    # We start with the root node whose parent is None
+                    # we don't want to graph the None node
+                    if parent:
+                        draw(parent, k)
+                    visit(v, k)
+                else:
+                    draw(parent, k)
+                    # drawing the label using a distinct name
+                    draw(k, k + '_' + v)
+
+        visit(self.tree.tree_structure)
+        os.makedirs(out_dir, exist_ok=True)
+        graph.write_png(os.path.join(
+            out_dir, f'decision_tree_{self.mode}.png'))
 
 
-class DecisionTreeClassifier(BasicTree):
+class DecisionTreeClassifier(BasicTreePredictor):
     def __init__(self, max_depth, scorer=GiniImportance):
-        super().__init__(max_depth, scorer)
-
-    def _get_best_split_for_feature(self, X_slice, y_slice):
-        X_slice_sorted = np.sort(X_slice)
-        thresholds = np.unique(
-            (X_slice_sorted[1:] + X_slice_sorted[:-1]) / 2)
-        current_fueature_score = self.scorer.get_score(y_slice)
-        best_score = current_fueature_score
-        best_threshold = None
-        for thresh in thresholds:
-            score = self.scorer.get_score_after_split(
-                X_slice, y_slice, thresh)
-            if score < best_score:
-                best_score = score
-                best_threshold = thresh
-        return best_score, best_threshold
-
-    def _get_best_split_for_dataset(self, X, y):
-        current_fueature_score = self.scorer.get_score(y)
-        best_score = current_fueature_score
-        for i in range(X.shape[1]):
-            X_slice = X[:, i]
-            best_score_for_feature, best_thresh_for_feature = self._get_best_split_for_feature(
-                X_slice, y)
-            if best_thresh_for_feature is not None and self.scorer.scoring_condition(best_score_for_feature, best_score):
-                best_score = best_score_for_feature
-                best_index = i
-                best_thresh = best_thresh_for_feature
-        return best_score, best_index, best_thresh
-
-    def get_output_format(self, score, samples_per_class, prediction, index=None, threshold=None):
-        root_key = f'{type(self.scorer).__name__} = {score:.2f}\nsamples = {np.sum(samples_per_class)}\nclass division = {samples_per_class}\nprediction = {prediction}'
-        if threshold is not None and index is not None:
-            root_key = f'feature_{index} < {threshold:.2f}\n' + root_key
-        return root_key
-
-    def _grow_tree(self, X, y, depth=0, tree_structure={}):
-        current_score = self.scorer.get_score(y)
-        samples_per_class = [np.sum(y == i)
-                             for i in range(1, self.number_of_classes + 1)]
-        predicted_class = np.argmax(samples_per_class) + 1
-        tree_node = TreeNode(
-            score=current_score, samples_per_class=samples_per_class, prediction=predicted_class)
-        if depth < self.max_depth:
-            best_score, best_index, best_thresh = self._get_best_split_for_dataset(
-                X, y)
-            if best_index is not None and best_thresh is not None:
-                left_mask = X[:, best_index] < best_thresh
-                right_mask = X[:, best_index] >= best_thresh
-                X_left = X[left_mask]
-                X_right = X[right_mask]
-                y_left = y[left_mask]
-                y_right = y[right_mask]
-                tree_node.feature_index = best_index
-                tree_node.threshold = best_thresh
-                root_key = self.get_output_format(
-                    best_score, samples_per_class, predicted_class, best_index, best_thresh)
-                tree_structure[root_key] = {}
-                tree_node.tree_structure = tree_structure
-                tree_node.left_node = self._grow_tree(
-                    X_left, y_left, depth + 1, tree_structure[root_key])
-                tree_node.right_node = self._grow_tree(
-                    X_right, y_right, depth + 1, tree_structure[root_key])
-        else:
-            root_key = self.get_output_format(
-                current_score, samples_per_class, predicted_class)
-            tree_structure[root_key] = {}
-            tree_node.tree_structure = tree_structure
-        return tree_node
+        super().__init__(max_depth, scorer,  mode='classification')
+        assert self.mode in self.scorer.optimization_types, f'scorer {self.scorer} is incompatible with this predictor'
 
     def fit(self, X, y):
         self.number_of_classes = len(set(y))
-        self.tree = self._grow_tree(X, y)
-
-    def _get_single_prediction(self, X_single):
-        assert self.tree is not None, 'Please first fit a tree to the data'
-
-        node = self.tree
-        while node.left_node:
-            if X_single[node.feature_index] < node.threshold:
-                node = node.left_node
-            else:
-                node = node.right_node
-        return node.prediction
-
-    def predict(self, X):
-        return [self._get_single_prediction(x) for x in X]
-
-    def draw_tree(self, out_dir):
-        assert self.tree is not None, 'Please first fit a tree to the data'
-
-        def draw(parent_name, child_name):
-            edge = pydot.Edge(parent_name, child_name)
-            graph.add_edge(edge)
-
-        def visit(node, parent=None):
-            for k, v in node.items():
-                if isinstance(v, dict):
-                    # We start with the root node whose parent is None
-                    # we don't want to graph the None node
-                    if parent:
-                        draw(parent, k)
-                    visit(v, k)
-                else:
-                    draw(parent, k)
-                    # drawing the label using a distinct name
-                    draw(k, k + '_' + v)
-
-        graph = pydot.Dot(graph_type='graph')
-        visit(self.tree.tree_structure)
-        graph.write_png(os.path.join(out_dir, 'decision_tree.png'))
+        super().fit(X, y)
 
 
-
-class DecisionTreeRegressor(BasicTree):
+class DecisionTreeRegressor(BasicTreePredictor):
     def __init__(self, max_depth, scorer=StandardDeviationReduction):
-        super().__init__(max_depth, scorer)
-
-    def _get_best_split_for_feature(self, X_slice, y_slice):
-        X_slice_sorted = np.sort(X_slice)
-        thresholds = np.unique(
-            (X_slice_sorted[1:] + X_slice_sorted[:-1]) / 2)
-        current_fueature_score = self.scorer.get_score(y_slice)
-        best_score = current_fueature_score
-        best_threshold = None
-        for thresh in thresholds:
-            score = self.scorer.get_score_after_split(
-                X_slice, y_slice, thresh)
-            if score < best_score:
-                best_score = score
-                best_threshold = thresh
-        return best_score, best_threshold
-
-    def _get_best_split_for_dataset(self, X, y):
-        current_fueature_score = self.scorer.get_score(y)
-        best_score = current_fueature_score
-        for i in range(X.shape[1]):
-            X_slice = X[:, i]
-            best_score_for_feature, best_thresh_for_feature = self._get_best_split_for_feature(
-                X_slice, y)
-            if best_thresh_for_feature is not None and self.scorer.scoring_condition(best_score_for_feature, best_score):
-                best_score = best_score_for_feature
-                best_index = i
-                best_thresh = best_thresh_for_feature
-        return best_score, best_index, best_thresh
-
-    def get_output_format(self, score, samples_per_class, prediction, index=None, threshold=None):
-        root_key = f'{type(self.scorer).__name__} = {score:.2f}\nsamples = {np.sum(samples_per_class)}\nclass division = {samples_per_class}\nprediction = {prediction}'
-        if threshold is not None and index is not None:
-            root_key = f'feature_{index} < {threshold:.2f}\n' + root_key
-        return root_key
-
-    def _grow_tree(self, X, y, depth=0, tree_structure={}):
-        current_score = self.scorer.get_score(y)
-        samples_per_class = None
-        predicted_value = np.mean(y)
-
-        tree_node = TreeNode(
-            score=current_score, samples_per_class=samples_per_class, prediction=predicted_value)
-        if depth < self.max_depth:
-            best_score, best_index, best_thresh = self._get_best_split_for_dataset(
-                X, y)
-            if best_index is not None and best_thresh is not None:
-                left_mask = X[:, best_index] < best_thresh
-                right_mask = X[:, best_index] >= best_thresh
-                X_left = X[left_mask]
-                X_right = X[right_mask]
-                y_left = y[left_mask]
-                y_right = y[right_mask]
-                tree_node.feature_index = best_index
-                tree_node.threshold = best_thresh
-                root_key = self.get_output_format(
-                    best_score, samples_per_class, predicted_value, best_index, best_thresh)
-                tree_structure[root_key] = {}
-                tree_node.tree_structure = tree_structure
-                tree_node.left_node = self._grow_tree(
-                    X_left, y_left, depth + 1, tree_structure[root_key])
-                tree_node.right_node = self._grow_tree(
-                    X_right, y_right, depth + 1, tree_structure[root_key])
-        else:
-            root_key = self.get_output_format(
-                current_score, samples_per_class, predicted_value)
-            tree_structure[root_key] = {}
-            tree_node.tree_structure = tree_structure
-        return tree_node
-
-    def fit(self, X, y):
-        self.number_of_classes = len(set(y))
-        self.tree = self._grow_tree(X, y)
-
-    def _get_single_prediction(self, X_single):
-        assert self.tree is not None, 'Please first fit a tree to the data'
-
-        node = self.tree
-        while node.left_node:
-            if X_single[node.feature_index] < node.threshold:
-                node = node.left_node
-            else:
-                node = node.right_node
-        return node.prediction
-
-    def predict(self, X):
-        return [self._get_single_prediction(x) for x in X]
-
-    def draw_tree(self, out_dir):
-        assert self.tree is not None, 'Please first fit a tree to the data'
-
-        def draw(parent_name, child_name):
-            edge = pydot.Edge(parent_name, child_name)
-            graph.add_edge(edge)
-
-        def visit(node, parent=None):
-            for k, v in node.items():
-                if isinstance(v, dict):
-                    # We start with the root node whose parent is None
-                    # we don't want to graph the None node
-                    if parent:
-                        draw(parent, k)
-                    visit(v, k)
-                else:
-                    draw(parent, k)
-                    # drawing the label using a distinct name
-                    draw(k, k + '_' + v)
-
-        graph = pydot.Dot(graph_type='graph')
-        visit(self.tree.tree_structure)
-        graph.write_png(os.path.join(out_dir, 'decision_tree.png'))
+        super().__init__(max_depth, scorer, mode='regression')
+        assert self.mode in self.scorer.optimization_types, f'scorer {self.scorer} is incompatible with this predictor'
--- random_forest/forest.py
+++ random_forest/forest.py
@@ -2,15 +2,15 @@ import numpy as np
 
 from scipy import stats
 
-from decision_trees.tree2 import DecisionTreeClassifier, DecisionTreeRegressor
+from decision_trees.tree import DecisionTreeClassifier, DecisionTreeRegressor
 
 
 class RandomForestBase:
-    def __init__(self, X, y, n_trees, max_depth, features_max_value_type='sqrt'):
-        # add random seed
+    def __init__(self, X, y, n_trees, max_depth, features_max_value_type='sqrt', rows_percentage=0.8, random_seed=42):
         self.X = X
         self.y = y
         self.n_trees = n_trees
+        self.rows_percentage = rows_percentage
         if features_max_value_type == 'sqrt':
             self.n_features = int(np.sqrt(self.X.shape[1]))
         elif features_max_value_type == 'log2':
@@ -18,15 +18,21 @@ class RandomForestBase:
         else:
             self.n_features = int(
                 features_max_value_type(self.X.shape[1]))
-        self.sampled_feature_indices = self._get_sampled_features_indices()
+        self.sampled_rows_indices, self.sampled_feature_indices = self._get_sampled_indices()
         self.trees = None
 
-    def _get_sampled_features_indices(self):
+    def _get_sampled_indices(self):
         sampled_feature_indices = []
+        sampled_rows_indices = []
         for _ in range(self.n_trees):
+            np.random.seed(random_seed)
             sampled_feature_indices.append(
                 np.random.permutation(self.X.shape[1])[:self.n_features])
-        return sampled_feature_indices
+            np.random.seed(random_seed)
+            sampled_rows_indices.append(
+                np.random.permutation(int(self.X.shape[0]*self.rows_percentage)))
+
+        return sampled_rows_indices, sampled_feature_indices
 
 
 class RandomForestClassifier(RandomForestBase):
@@ -36,13 +42,13 @@ class RandomForestClassifier(RandomForestBase):
             max_depth=max_depth) for _ in range(self.n_trees)]
 
     def fit(self):
-        for feature_indices, tree in zip(self.sampled_feature_indices, self.trees):
-            tree.fit(self.X[:, feature_indices], self.y)
+        for row_indices, feature_indices, tree in zip(self.sampled_rows_indices, self.sampled_feature_indices, self.trees):
+            tree.fit(self.X[row_indices][:, feature_indices], self.y)
 
-    def predict(self):
+    def predict(self, X_test):
         predictions = []
         for feature_indices, tree in zip(self.sampled_feature_indices, self.trees):
-            predictions.append(tree.predict(self.X[:, feature_indices]))
+            predictions.append(tree.predict(X_test[:, feature_indices]))
         predictions = stats.mode(np.array(predictions), axis=0)
         return predictions
 
@@ -54,12 +60,11 @@ class RandomForestRegressor(RandomForestBase):
             max_depth=max_depth) for _ in range(self.n_trees)]
 
     def fit(self):
-        for feature_indices, tree in zip(self.sampled_feature_indices, self.trees):
-            tree.fit(self.X[:, feature_indices], self.y)
+        for row_indices, feature_indices, tree in zip(self.sampled_rows_indices, self.sampled_feature_indices, self.trees):
+            tree.fit(self.X[row_indices][:, feature_indices], self.y)
 
-    def predict(self):
+    def predict(self, X_test):
         predictions = []
         for feature_indices, tree in zip(self.sampled_feature_indices, self.trees):
-            predictions.append(tree.predict(self.X[:, feature_indices]))
-        predictions = np.array(predictions).mean(0)
+            predictions.append(tree.predict(X_test[:, feature_indices]))        predictions = np.array(predictions).mean(0)
         return predictions
